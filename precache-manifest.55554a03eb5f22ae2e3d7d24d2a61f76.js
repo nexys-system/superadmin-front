a5306de0410f125f8171df3578b5f311f64d8d68
johnb8005 <johnb8005@users.noreply.github.com>
2021-08-03 16:51:11 +0000

    deploy: 49f4e3dbc837621ae8ec2a4a87ff6992d31a4c07

---
+++ precache-manifest.55554a03eb5f22ae2e3d7d24d2a61f76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6618456a0cbb45804635a5090543c9a1",
+    "url": "/react-boilerplate-ts/index.html"
+  },
+  {
+    "revision": "e7412a32c27f3f47035f",
+    "url": "/react-boilerplate-ts/static/js/2.53938377.chunk.js"
+  },
+  {
+    "revision": "05cba21318d5e1fcc0e0f1bfca1a684f",
+    "url": "/react-boilerplate-ts/static/js/2.53938377.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c840a61f9be21b9466c2",
+    "url": "/react-boilerplate-ts/static/js/main.d3d939fc.chunk.js"
+  },
+  {
+    "revision": "d1089f0a987b644dfb6b",
+    "url": "/react-boilerplate-ts/static/js/runtime-main.803a317f.js"
+  }
+]);
